4e0f8f77138adbcc60d4fdf00a43d99a9cac6886
Nagaraj <nagarajcheera18@gmail.com>
2026-02-05 19:32:09 +0530

    first commit

---
+++ 6-alternative-flavor.js
@@ -0,0 +1,11 @@
+// similar to module.exports = {items}
+module.exports.items = ['items1', 'item2']
+const person = {
+  name : 'bob',   
+}
+
+// similar to module.exports = {person}
+module.exports.singlePerson = person
+
+
+console.log(module)
