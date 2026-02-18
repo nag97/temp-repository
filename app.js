d74ef9ad3d8000b2aeda29c8355a4c72a2f8e4ed
Nagaraj <nagarajcheera18@gmail.com>
2026-02-18 06:55:49 +0530

    understanding async patterns

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-const http = require('http')
+const { readFile, writeFile} = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
-const server = http.createServer((req, res) => {
-  console.log('request event')
-  res.end('Hello World')
-})
+console.log('first')
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", 'utf-8');
+    const second = await readFile("./content/second.txt", 'utf8');
+    await writeFile('./content/result-mind-grenade.txt', `This is awesome: ${first} ${second}`, {flag: 'a'})
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-server.listen(5000, () => {
-  console.log('Server listening on port : 5000....')
-})
+
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
